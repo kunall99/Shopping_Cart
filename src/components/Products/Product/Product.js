059e9ddaaffc902928870b15b19cca5ca83e3df8
Kunal Gupta <gkunal933@gmail.com>
2021-10-11 14:51:11 +0530

    updated

--- src/components/Products/Product/Product.js
+++ src/components/Products/Product/Product.js
@@ -16,64 +16,64 @@ import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 import './Product.css'
 const useStyles = makeStyles({
-    root: {
-      maxWidth: 345,
-      marginBottom:'5%'
-      
-    },
-    media: {
-      height: '40vh',
-    },
-  });
+  root: {
+    maxWidth: 345,
+    marginBottom: '5%'
+
+  },
+  media: {
+    height: '40vh',
+  },
+});
 
 function Product2({ product, addToCart, loadCurrentItem }) {
   const history = useHistory();
-    const classes = useStyles();
-    const handleOnClick = ()=>{
-      loadCurrentItem(product);
-      history.push(`/product/${product.id}`)
-    }
-    return (
-        <Card className={classes.root}>
-      
-          <CardMedia
-            className={classes.media}
-            image={product.image}
-            title={product.title}
-            
-          />
-          <CardContent className={classes.cardstyle}>
-            <Typography gutterBottom variant="h5" component="h2">
-              {product.title}
-            </Typography>
-            <Typography variant="body2" color="textSecondary" component="p" style={{height: '26vh'}}>
-              {product.description}
-            </Typography>
-            <br/>
-            <Typography variant="h5" align='center' color="textPrimary" >
-              {product.price}&nbsp;₹
-            </Typography>
-          </CardContent>
-       
-        <CardActions >
-      
-          <Button  size="small" color="primary" onClick={handleOnClick}>
-            View Item
-          </Button>
-          
-          <Button size="small" color="primary" onClick={() => addToCart(product.id)}>
-            Add to Cart
-          </Button>
-        </CardActions>
-      </Card>
-    )
+  const classes = useStyles();
+  const handleOnClick = () => {
+    loadCurrentItem(product);
+    history.push(`/product/${product.id}`)
+  }
+  return (
+    <Card className={classes.root}>
+
+      <CardMedia
+        className={classes.media}
+        image={product.image}
+        title={product.title}
+
+      />
+      <CardContent className={classes.cardstyle}>
+        <Typography gutterBottom variant="h5" component="h2">
+          {product.title}
+        </Typography>
+        <Typography variant="body2" color="textSecondary" component="p" style={{ height: '26vh' }}>
+          {product.description}
+        </Typography>
+        <br />
+        <Typography variant="h5" align='center' color="textPrimary" >
+        ₹ {product.price}
+        </Typography>
+      </CardContent>
+
+      <CardActions >
+
+        <Button size="small" color="primary" onClick={handleOnClick}>
+          View Item
+        </Button>
+
+        <Button size="small" color="primary" onClick={() => addToCart(product.id)}>
+          Add to Cart
+        </Button>
+      </CardActions>
+    </Card>
+  )
 }
 
 const mapDispatchToProps = (dispatch) => {
-    return {
-      addToCart: (id) => dispatch(addToCart(id)),
-      loadCurrentItem: (item) => dispatch(loadCurrentItem(item)),
-    };
+  return {
+    addToCart: (id) => dispatch(addToCart(id)),
+    loadCurrentItem: (item) => dispatch(loadCurrentItem(item)),
   };
-  
-  export default connect(null, mapDispatchToProps)(Product2);
+};
+
+export default connect(null, mapDispatchToProps)(Product2);
