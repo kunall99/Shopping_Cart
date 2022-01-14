1a76759dc128050a045be31bc5a679ab45b00bd0
Kunal Gupta <gkunal933@gmail.com>
2022-01-14 22:44:52 +0530

    Updates

---
+++ precache-manifest.edd676615d3be6d19da1c70cdf4b8322.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f8dc9bb3bc16a03281878b57ecfbea7",
+    "url": "/Shopping_App/index.html"
+  },
+  {
+    "revision": "20805895085b41a54f98",
+    "url": "/Shopping_App/static/css/main.705e556d.chunk.css"
+  },
+  {
+    "revision": "3f33ecfb024532cb56be",
+    "url": "/Shopping_App/static/js/2.df1d1bf6.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Shopping_App/static/js/2.df1d1bf6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "20805895085b41a54f98",
+    "url": "/Shopping_App/static/js/main.00977edb.chunk.js"
+  },
+  {
+    "revision": "6e85070ca7793ec2ef68",
+    "url": "/Shopping_App/static/js/runtime-main.ff0bc6a2.js"
+  },
+  {
+    "revision": "028978f40ca009a385c6fb19486bdc53",
+    "url": "/Shopping_App/static/media/book.028978f4.jpg"
+  },
+  {
+    "revision": "67a487675e594f3b274f76d190aa011f",
+    "url": "/Shopping_App/static/media/smartphone.67a48767.jpg"
+  },
+  {
+    "revision": "4a0dbe85527b88bfcea004afbf6c9fb8",
+    "url": "/Shopping_App/static/media/speaker.4a0dbe85.jpg"
+  }
+]);
